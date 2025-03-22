fe98892e62be0edd8958eb94b12e239d1e79acfc
Huggo Parcelly <h.parcelly@gmail.com>
2025-03-21 22:19:52 -0300

    start project

---
+++ app/src/main/java/com/example/weathernow/OpenWeatherService.kt
@@ -0,0 +1,17 @@
+package com.example.weathernow
+
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface OpenWeatherService {
+
+    //    https://api.openweathermap.org/data/2.5/
+    //    weather?q={city name}&appid={API key}
+
+    @GET("weather")
+    fun getCurrentWeatherData(
+        @Query("q") cityName: String,
+        @Query("appid") apiKey: String,
+        @Query("units") units: String = "metric"
+    )
+}
